e866c3763acb0a2f4badde210f8e44700ffb2de2
Mohammad Talha <talhaviet22@gmail.com>
2020-03-19 11:38:21 +0530

    OTP Screen

--- lib/bloc/validator.dart
+++ lib/bloc/validator.dart
@@ -1,43 +1,38 @@
 import 'dart:async';
 
-class BlocValidator{
-
+class BlocValidator {
   var mobileValidator = StreamTransformer<String, String>.fromHandlers(
-    handleData: (mobile, sink){
-      if(mobile.trim().length == 10){
-        sink.add(mobile);
-      }else{
-        sink.addError("Invalid Mobile Number");
-      }
+      handleData: (mobile, sink) {
+    if (mobile.trim().length == 10) {
+      sink.add(mobile);
+    } else {
+      sink.addError("Invalid Mobile Number");
     }
-  );
+  });
 
-var nameValidator = StreamTransformer<String, String>.fromHandlers(
-    handleData: (name, sink){
-      if(name.trim().length > 0){
-        sink.add(name);
-      }else{
-        sink.addError("Provide Full Name");
-      }
+  var nameValidator =
+      StreamTransformer<String, String>.fromHandlers(handleData: (name, sink) {
+    if (name.trim().length > 0) {
+      sink.add(name);
+    } else {
+      sink.addError("Provide Full Name");
     }
-  );
+  });
 
-  var emailValidator = StreamTransformer<String, String>.fromHandlers(
-    handleData: (email, sink){
-      if(email.contains("@")){
-        sink.add(email);
-      }else{
-        sink.addError("Invalid Email");
-      }
+  var emailValidator =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains("@")) {
+      sink.add(email);
+    } else {
+      sink.addError("Invalid Email");
     }
-  );
+  });
   var passwordValidator = StreamTransformer<String, String>.fromHandlers(
-    handleData: (password, sink){
-      if(password.length >= 8){
-        sink.add(password);
-      }else{
-        sink.addError("Password should be minimum 8 characters");
-      }
+      handleData: (password, sink) {
+    if (password.length >= 8) {
+      sink.add(password);
+    } else {
+      sink.addError("Password should be minimum 8 characters");
     }
-  );
+  });
 }
--- lib/screens/otp_verification_screen.dart
+++ lib/screens/otp_verification_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:pin_entry_text_field/pin_entry_text_field.dart';
 
 class OtpScreen extends StatefulWidget {
   @override
@@ -8,8 +10,114 @@ class OtpScreen extends StatefulWidget {
 class _OtpScreenState extends State<OtpScreen> {
   @override
   Widget build(BuildContext context) {
+    final _height = MediaQuery.of(context).size.height;
+    final _width = MediaQuery.of(context).size.width;
+    bool isFilled = false;
+
     return Scaffold(
-      body: Container(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
+      ),
+      body: SafeArea(
+          child: Container(
+        height: _height,
+        width: _width,
+        color: Colors.white,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text("Welcome to",
+                    style: TextStyle(
+                      fontFamily: "Maven",
+                      fontSize: 16,
+                      color: Colors.grey,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text("DREAM PROJECT",
+                    style: TextStyle(
+                      fontFamily: "Maven",
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    )),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: Text("VERIFY MOBILE NUMBER",
+                    style: TextStyle(
+                      fontFamily: "Maven",
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.black,
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Text(
+                    "We have sent you an access code \n via SMS for Mobile number verification",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontFamily: "Maven",
+                      fontSize: 14,
+                      color: Colors.black,
+                    )),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              PinEntryTextField(
+                fields: 6,
+                onSubmit: (String pin) {
+                  if (pin.length == 6) {
+                    setState(() {
+                      isFilled = true;
+                    });
+                  }
+                  print(isFilled);
+                },
+              ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: RaisedButton(
+                    elevation: 4,
+                    onPressed: () {
+                      print(isFilled);
+                      if (!isFilled) {
+                        Fluttertoast.showToast(msg: "Fill otp");
+                        return;
+                      }
+                    },
+                    color: Color(0xff60B243),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15),
+                      child: Center(
+                          child: Text(
+                        "VERIFY",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: "Maven",
+                        ),
+                      )),
+                    )),
+              ),
+            ]),
+      )),
     );
   }
 }
